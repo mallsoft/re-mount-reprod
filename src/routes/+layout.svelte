70f1eb62952d2cf2cbf195e2e36f8b61464b537b
hans <hkmelaa@hotmail.com>
2022-11-22 23:15:10 +0100

    initial

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+	import '../global.css';
+	import { navigating } from '$app/stores';
+</script>
+
+<svelte:head>
+	<title>Wierd</title>
+</svelte:head>
+
+{#if $navigating}
+	<span>spagetti?</span>
+{/if}
+
+<slot />
---
+++ src/routes/+page.svelte
@@ -0,0 +1,31 @@
+<main>
+	<h1>Welcome to FunkyKit</h1>
+	<p>
+		bad => Refresh and see animation play twice (it shouldn't) <br />
+		Ok so the issue seems to have something with what's in layout...
+	</p>
+</main>
+
+<style>
+	main {
+		min-height: 100vh;
+		background-color: rgb(231, 231, 231);
+
+		display: flex;
+		justify-content: center;
+		align-items: center;
+
+		flex-direction: column;
+	}
+
+	/* this is where things go south */
+	h1 {
+		animation: intro 0.5s;
+	}
+
+	@keyframes intro {
+		from {
+			transform: translateY(-50px);
+		}
+	}
+</style>
